621915b9ef2dba7083c717a281f283b296646a7b
WilliamAvilaArmijos <williamavilaarmijos@hotmail.com>
2022-06-08 12:23:01 -0500

    Add files via upload

---
+++ calculadora/Calc/Calc/Calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calc<T>: ICalc<T>
+    {
+        public T Division(T valor1, T valor2)
+        {
+            return (dynamic)valor1 / valor2;
+        }
+
+        public T Multiplicacion(T valor1, T valor2)
+        {
+            return (dynamic)valor1 * valor2;
+        }
+
+        public T Resta(T valor1, T valor2)
+        {
+            return (dynamic)valor1 - valor2;
+        }
+
+        public T Suma(T valor1, T valor2 )
+        {
+            return (dynamic)valor1 + valor2;
+        }
+    }
+}
---
+++ calculadora/Calc/Calc/Program.cs
@@ -0,0 +1,16 @@
+﻿using Calc;
+double val1, val2;
+
+Calc<double> calc = new Calc<double>();
+Console.WriteLine("Ingrese un numero");
+val1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese un numero");
+val2 = double.Parse(Console.ReadLine());
+double sum = calc.Suma(val1, val2);
+double res = calc.Resta(val1, val2);
+double mul = calc.Multiplicacion(val1, val2);
+double div = calc.Division(val1, val2);
+Console.WriteLine("La suma de "+ val1 + "+" + val2 +" es: " +sum);
+Console.WriteLine("La resta de " + val1 + "-" + val2 + " es: " + res);
+Console.WriteLine("La multiplicacion de " + val1 + "*" + val2 + " es: " + mul);
+Console.WriteLine("La division de " + val1 + "/" + val2 + " es: " + div);
